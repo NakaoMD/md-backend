c8c85a20e1547b895704d31fa8ac36ad0996f442
NakaoMD <nakao.mds@gmail.com>
2025-04-24 03:36:21 -0300

    swagger

--- src/main/java/br/com/md/config/SwaggerConfig.java
+++ src/main/java/br/com/md/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package br.com.md.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server; // <- Importação correta
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,12 +17,13 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("https://md-backend-production.up.railway.app")))
                 .info(new Info()
                         .title("API MD BACKEND")
                         .version("1.0")
-                        .description("Documentação das APIs MD-BACKEND."))
+                        .description("Documentação das APIs MD-BACKEND"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
+                .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
                                         .name("bearerAuth")
@@ -26,4 +31,5 @@ public class SwaggerConfig {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
     }
+
 }
