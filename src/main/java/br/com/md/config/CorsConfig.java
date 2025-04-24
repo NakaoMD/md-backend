2b6b2974c2f7e7e4d30d40709f938993e610b09d
NakaoMD <nakao.mds@gmail.com>
2025-04-24 03:26:09 -0300

    cors

---
+++ src/main/java/br/com/md/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package br.com.md.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // aplica para todas as rotas
+                        .allowedOrigins("*") // ou especifique: "https://seusite.com"
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
